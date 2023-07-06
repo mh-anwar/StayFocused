fabe396424f1315a2735ddaa62fc47af0fea0b96
mh-anwar <mohammad.anwar2939@gmail.com>
2023-07-06 15:22:37 -0400

    Update Banned Sites Interface

--- options/banned_sites/index.js
+++ options/banned_sites/index.js
@@ -1,4 +1,3 @@
-let submitBannedSites = document.getElementById('submit_banned_sites');
 let bannedSitesInput = document.getElementById('banned_sites_list');
 let commonSitesView = document.getElementById('common_sites_view');
 const bannedSitesView = document.getElementById('banned_sites_view');
@@ -57,12 +56,12 @@ async function updateBannedSites() {
 function updateCommonSites() {
     for (let i = 0; i < commonBanned.length; i++) {
         let site = commonBanned[i];
-        let siteDiv = document.createElement('div');
-        siteDiv.innerText = site;
-        siteDiv.addEventListener('click', (e) =>
+        let siteRow = document.createElement('div');
+        siteRow.innerText = site;
+        siteRow.addEventListener('click', (e) =>
             addBannedSite(e.target.innerText, e)
         );
-        commonSitesView.appendChild(siteDiv);
+        commonSitesView.appendChild(siteRow);
     }
 }
 
@@ -96,34 +95,58 @@ function displayMyBannedSites() {
         let sites =
             typeof data.focusSites === 'undefined' ? {} : data.focusSites;
         sites = Object.keys(sites);
+        displayAddBannedSite();
 
         for (let i = 0; i < sites.length; i++) {
             let site = sites[i];
-            let siteDiv = document.createElement('tr');
-            let siteRemove = document.createElement('td');
-            siteRemove.innerText = '❌  '; // yes sometimes we have to use emojis
+            let siteRow = document.createElement('tr');
+
             let siteText = document.createElement('td');
             siteText.innerText = site;
 
-            let siteTiming = document.createElement('td');
-            let siteTime = document.createElement('input');
-            siteTime.type = 'number';
-            siteTime.value = 15;
-            siteTiming.appendChild(siteTime);
+            siteRow.appendChild(createSiteRemoveInput());
+            siteRow.appendChild(siteText);
+            siteRow.appendChild(createTimeInput());
 
-            siteRemove.addEventListener('click', (e) =>
-                removeBannedSite(site, e)
-            );
-
-            siteDiv.appendChild(siteRemove);
-            siteDiv.appendChild(siteText);
-            siteDiv.appendChild(siteTiming);
-
-            bannedSitesView.appendChild(siteDiv);
+            bannedSitesView.appendChild(siteRow);
         }
     });
 }
 
-submitBannedSites.addEventListener('click', updateBannedSites);
+function displayAddBannedSite() {
+    let addSiteRow = document.createElement('tr');
+    let addSiteText = document.createElement('td');
+    addSiteText.innerText = '➕';
+    addSiteText.addEventListener('click', updateBannedSites);
+
+    let addSiteInfo = document.createElement('td');
+    let addSiteInput = document.createElement('input');
+    addSiteInput.type = 'text';
+    addSiteInput.id = 'banned_sites_list';
+    addSiteInfo.appendChild(addSiteInput);
+
+    addSiteInput.placeholder = 'Enter a site per line';
+    addSiteRow.appendChild(addSiteText);
+    addSiteRow.appendChild(addSiteInfo);
+    addSiteRow.appendChild(createTimeInput());
+    bannedSitesView.appendChild(addSiteRow);
+}
+
+const createTimeInput = () => {
+    let siteTiming = document.createElement('td');
+    let siteTime = document.createElement('input');
+    siteTime.type = 'number';
+    siteTime.value = 15;
+    siteTiming.appendChild(siteTime);
+    return siteTiming;
+};
+
+const createSiteRemoveInput = () => {
+    let siteRemove = document.createElement('td');
+    siteRemove.innerText = '❌  '; // yes sometimes we have to use emojis
+    siteRemove.addEventListener('click', (e) => removeBannedSite(site, e));
+    return siteRemove;
+};
+
 updateCommonSites();
 displayMyBannedSites();
